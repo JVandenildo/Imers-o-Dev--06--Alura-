f45785608b6b01e06e1c34dcacede4d110f8ead1
JVandenildo <jvandenildo@gmail.com>
2023-01-31 14:54:02 -0300

    Aula02 com tentativas

--- Aula 02/Aula02.js
+++ Aula 02/Aula02.js
@@ -2,25 +2,51 @@ const valor1 = document.querySelector(".valor1");
 const valorTeste = document.querySelector(".valorTeste");
 const feelingLucky = document.querySelector('#feelingLucky');
 feelingLucky.addEventListener("click", testingLuck);
-const luckyNumber = document.querySelector("#luckyNumber");
-luckyNumber.addEventListener("click", luckyNumberGet);
 const entrada = document.querySelector('#entrada');
+const luckyTip = document.querySelector('.luckyTip');
+// const luckyButton = document.querySelector('#luckyButton');
+// luckyButton.addEventListener("click", gettingLuck);
+var luckyCount = 0;
+var luckyCountMax = 10;
+// declarando o número a ser acertado
 
-function luckyNumberGet(){
-    let date = new Date();
-    const randomNumber = Math.round(10*(Math.random(date)));
-    valorTeste.innerHTML = `${randomNumber}`;
-    return randomNumber;
+var min = 1;
+var max = 10000;
+luckyTip.innerHTML = `Seu número está de ${min} a ${max}.`;
+const luckyNumber = gettingLuck();
+function gettingLuck(){
+    const date = new Date();
+    return Math.floor(Math.random(date) * (max - min + 1)) + min;
 }
 
+// função do botão
 function testingLuck(){
-    var luckyCount = 0;
-    if(entrada.value == luckyNumberGet()){
-        valor1.innerHTML = `Acertou! O número da sorte é ${luckyNumberGet()}.`
-        entrada.value = "";
-    }else{
-        valor1.innerHTML = `Tente novamente ${luckyNumberGet()}.`;
+    valorTeste.innerHTML = `${luckyNumber}`;
+    if(entrada.value == luckyNumber){
         luckyCount++;
         entrada.value = "";
+        if(luckyCount == 1){
+            valor1.innerHTML = `Acertou após ${luckyCount} tentativa!`;
+        }else{
+            valor1.innerHTML = `Acertou após ${luckyCount} tentativas!`;
+        }
+    }
+    else if(entrada.value < luckyNumber){
+        luckyCount++;
+        valor1.innerHTML = `Insira um número válido e tente mais uma vez! Ainda é a ${luckyCount}ª vez que você tenta, o número é maior do que ${entrada.value}.`;
+        entrada.value = "";
+        if(luckyCount > luckyCountMax){
+            valor1.innerHTML = `Sinto muito, suas tentativas acabaram. O número era ${luckyNumber}.`;
+            feelingLucky.removeEventListener("click", testingLuck);
+        }
+    }
+    else if(entrada.value > luckyNumber){
+        luckyCount++;
+        valor1.innerHTML = `Insira um número válido e tente mais uma vez! Ainda é a ${luckyCount}ª vez que você tenta, o número é menor do que ${entrada.value}.`;
+        entrada.value = "";
+        if(luckyCount > luckyCountMax){
+            valor1.innerHTML = `Sinto muito, suas tentativas acabaram. O número era ${luckyNumber}.`;
+            feelingLucky.removeEventListener("click", testingLuck);
+        }
     }
 }

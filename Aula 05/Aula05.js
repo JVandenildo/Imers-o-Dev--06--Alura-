732c55ddcc3fa758a56a3198075d90dda0609165
JVandenildo <jvandenildo@gmail.com>
2023-02-05 17:03:09 -0300

    right/wrong buttons + functions

--- Aula 05/Aula05.js
+++ Aula 05/Aula05.js
@@ -40,7 +40,7 @@ function mostrarJogador(){
             Jogador01.nome = jogadoresNomes[0];
             var table = `<table>
             <tr>
-                <td><b>Nome:</b> ${Jogador01.nome}</td><td><b>Pontos:</b> ${Jogador01.pontos}</td><td><b>Acertos:</b> ${Jogador01.acertos}</td><td><b>Erros:</b> ${Jogador01.erros}</td>
+                <td><b>Nome:</b> ${Jogador01.nome}</td><td><b>Pontos:</b> ${Jogador01.pontos}</td><td><b>Acertos:</b> ${Jogador01.acertos}</td><td><b>Erros:</b> ${Jogador01.erros}</td><td><button onclick="acertei(Jogador01)">ACERTEI</button></td><td><button onclick="errei(Jogador01)">ERREI</button></td>
             </tr>
         </table>`;
             participantes.insertAdjacentHTML("beforeend", table);
@@ -50,10 +50,10 @@ function mostrarJogador(){
             Jogador02.nome = jogadoresNomes[1];
             var table = `<table>
             <tr>
-                <td><b>Nome:</b> ${Jogador01.nome}</td><td><b>Pontos:</b> ${Jogador01.pontos}</td><td><b>Acertos:</b> ${Jogador01.acertos}</td><td><b>Erros:</b> ${Jogador01.erros}</td>
+                <td><b>Nome:</b> ${Jogador01.nome}</td><td><b>Pontos:</b> ${Jogador01.pontos}</td><td><b>Acertos:</b> ${Jogador01.acertos}</td><td><b>Erros:</b> ${Jogador01.erros}</td><td><button onclick="acertei(Jogador01)">ACERTEI</button></td><td><button onclick="errei(Jogador01)">ERREI</button></td>
             </tr>
             <tr>
-                <td><b>Nome:</b> ${Jogador02.nome}</td><td><b>Pontos:</b> ${Jogador02.pontos}</td><td><b>Acertos:</b> ${Jogador02.acertos}</td><td><b>Erros:</b> ${Jogador02.erros}</td>
+                <td><b>Nome:</b> ${Jogador02.nome}</td><td><b>Pontos:</b> ${Jogador02.pontos}</td><td><b>Acertos:</b> ${Jogador02.acertos}</td><td><b>Erros:</b> ${Jogador02.erros}</td><td><button onclick="acertei(Jogador02)">ACERTEI</button></td><td><button onclick="errei(Jogador02)">ERREI</button></td>
             </tr>
         </table>`;
             participantes.insertAdjacentHTML("beforeend", table);
@@ -62,13 +62,13 @@ function mostrarJogador(){
             Jogador03.nome = jogadoresNomes[2];
             var table = `<table>
             <tr>
-                <td><b>Nome:</b> ${Jogador01.nome}</td><td><b>Pontos:</b> ${Jogador01.pontos}</td><td><b>Acertos:</b> ${Jogador01.acertos}</td><td><b>Erros:</b> ${Jogador01.erros}</td>
+                <td><b>Nome:</b> ${Jogador01.nome}</td><td><b>Pontos:</b> ${Jogador01.pontos}</td><td><b>Acertos:</b> ${Jogador01.acertos}</td><td><b>Erros:</b> ${Jogador01.erros}</td><td><button onclick="acertei(Jogador01)">ACERTEI</button></td><td><button onclick="errei(Jogador01)">ERREI</button></td>
             </tr>
             <tr>
-                <td><b>Nome:</b> ${Jogador02.nome}</td><td><b>Pontos:</b> ${Jogador02.pontos}</td><td><b>Acertos:</b> ${Jogador02.acertos}</td><td><b>Erros:</b> ${Jogador02.erros}</td>
+                <td><b>Nome:</b> ${Jogador02.nome}</td><td><b>Pontos:</b> ${Jogador02.pontos}</td><td><b>Acertos:</b> ${Jogador02.acertos}</td><td><b>Erros:</b> ${Jogador02.erros}</td><td><button onclick="acertei(Jogador02)">ACERTEI</button></td><td><button onclick="errei(Jogador02)">ERREI</button></td>
             </tr>
             <tr>
-                <td><b>Nome:</b> ${Jogador03.nome}</td><td><b>Pontos:</b> ${Jogador03.pontos}</td><td><b>Acertos:</b> ${Jogador03.acertos}</td><td><b>Erros:</b> ${Jogador03.erros}</td>
+                <td><b>Nome:</b> ${Jogador03.nome}</td><td><b>Pontos:</b> ${Jogador03.pontos}</td><td><b>Acertos:</b> ${Jogador03.acertos}</td><td><b>Erros:</b> ${Jogador03.erros}</td><td><button onclick="acertei(Jogador03)">ACERTEI</button></td><td><button onclick="errei(Jogador03)">ERREI</button></td>
             </tr>
         </table>`;
             participantes.insertAdjacentHTML("beforeend", table);
@@ -119,4 +119,17 @@ function limpaValue(){
 // uppercase the first letter
 function capitalLetter(string){
     return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+function acertei(jogador){
+    jogador.acertos = jogador.acertos + 1;
+    jogador.pontos = jogador.pontos + 2;
+
+    alert(`${jogador.nome}${jogador.acertos}`);
+}
+
+function errei(jogador){
+    jogador.erros = jogador.erros + 1;
+
+    alert(`${jogador.nome}${jogador.erros}`);
 }
